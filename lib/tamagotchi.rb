e763f73282bee0772149c8d488b642f18983bbba
Diego Suarez and Jim Padilla <diego@>
2016-11-29 11:40:37 -0800

    maintenance function for tamagotchi

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -2,13 +2,24 @@ class Tamagotchi
 
   define_method(:initialize) do |name|
     @name =  name
-    @food_level = 10
-    @sleep_level = 10
-    @activity_level = 10
+    @food_level = 20
+    @sleep_level = 20
+    @activity_level = 20
+    @birth = Time.now
+    every_so_many_seconds(1) do
+      @food_level -= 1
+      if !self.is_alive?
+        break
+      end
+    end
+
   end
 
   define_method(:name) do
     @name
+  end
+  define_method(:birth) do
+    @birth
   end
 
   define_method(:food_level) do
@@ -34,4 +45,56 @@ class Tamagotchi
   define_method(:kill) do
     @food_level = 0
   end
+
+  define_method(:feed) do
+    @food_level+=5
+    @activity_level-=3
+    @sleep_level-=3
+    #     Ingredients
+    # 1 pound salted cod fish
+    # 4 potatoes, sliced thick
+    # 2 onions, sliced
+    # 4 hard-boiled eggs, sliced
+    # 2 teaspoons capers
+    # 2 large cloves garlic, minced
+    # 1/4 cup pitted green olives
+    #
+    # 1 (4 ounce) jar roasted red bell peppers, drained
+    # 1/2 cup golden raisins
+    # 1 bay leaf
+    # 1 (8 ounce) can tomato sauce
+    # 1/2 cup extra virgin olive oil
+    # 1 cup water
+    # 1/4 cup white wine
+    # Directions
+    # Soak the salted cod in about 2 quarts of water, changing the water 3 times over the course of 8 hours. Drain and cut the fish into bite-size pieces.
+    # Layer the half of each ingredient in the following order: potatoes, cod fish, onions, hard-boiled eggs, capers, garlic, olives, roasted red peppers, and raisins. Place the bay leaf on top, then pour half the tomato sauce and half the olive oil. Repeat with the remaining ingredients in the same order. Pour the water and white wine on top. Do not stir.
+    # Cover and bring to a boil over medium heat. Reduce heat to medium-low and simmer until the potatoes are tender, about 30 minutes.
+  end
+
+  define_method(:play) do
+    @food_level-=4
+    @activity_level+=5
+    @sleep_level-=3
+  end
+
+  define_method(:sleep) do
+    @food_level-=5
+    @activity_level-=4
+    @sleep_level+=10
+  end
+
+  def every_so_many_seconds(seconds)
+    last_tick = Time.now
+    loop do
+      sleep 0.1
+      if Time.now - last_tick >= seconds
+        last_tick += seconds
+        yield
+      end
+    end
+  end
+
 end
+#
+# a = Tamagotchi.new('diego')
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -3,16 +3,16 @@ require('tamagotchi')
 
 describe(Tamagotchi) do
   describe('#initialize') do
-    it('sets the name and life levels fo a new Tamagotchi') do
+    it('sets the name and life levels for a new Tamagotchi') do
       my_pet = Tamagotchi.new('ddragon')
       expect(my_pet.name()).to(eq('ddragon'))
-      expect(my_pet.food_level()).to(eq(10))
-      expect(my_pet.sleep_level()).to(eq(10))
-      expect(my_pet.activity_level()).to(eq(10))
+      expect(my_pet.food_level()).to(eq(20))
+      expect(my_pet.sleep_level()).to(eq(20))
+      expect(my_pet.activity_level()).to(eq(20))
+      #expect(my_pet.birth()).to(eq(Time.now))
     end
   end
   describe('#is_alive?') do
-
     it "is alive if the food level is above 0" do
       my_pet = Tamagotchi.new('jdragon')
       expect(my_pet.is_alive?()).to(eq(true))
@@ -29,6 +29,34 @@ describe(Tamagotchi) do
       expect(my_pet.is_alive?()).to(eq(false))
     end
   end
+  describe('#play') do
+    it('raises activity_level') do
+      my_pet = Tamagotchi.new('jdragon')
+      my_pet.play()
+      expect(my_pet.activity_level()).to(eq(25))
+    end
+  end
+  describe('#eat') do
+    it('raises food_level') do
+      my_pet = Tamagotchi.new('jdragon')
+      my_pet.feed()
+      expect(my_pet.food_level()).to(eq(25))
+    end
+  end
+  describe('#sleep') do
+    it('raises sleep_level') do
+      my_pet = Tamagotchi.new('jdragon')
+      my_pet.sleep()
+      expect(my_pet.sleep_level()).to(eq(30))
+    end
+  end
+
+  # describe('#time_passes') do
+  #   it('decreases the amount of food the Tamagotchi has left by 1') do
+  #     my_pet =  Tamagotchi.new('ddragon')
+  #     expect(my_pet.food_level()).to(eq((Time.now-my_pet.birth).floor))
+  #   end
+  # end
 
 
 end
