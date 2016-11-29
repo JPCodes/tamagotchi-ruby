a70eee47f1c8a71628a6fcf72a512684ee8d8429
Diego Suarez and Jim Padilla <diego@>
2016-11-29 13:37:53 -0800

    Add more button functionality and css

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('./lib/**/*.rb')
 require('./lib/tamagotchi.rb')
+require('haml')
+
 
 get('/') do
   erb(:index)
@@ -17,15 +19,41 @@ post('/tamagotchi-form') do
 end
 
 post('/feed') do
+  @name=$new_tamagotchi.name()
   $new_tamagotchi.feed()
   @food = $new_tamagotchi.food_level()
   @sleep = $new_tamagotchi.sleep_level()
   @activity = $new_tamagotchi.activity_level()
 
-  if @food < 0 or @sleep < 0 or @activity < 0
+  if @food <= 0 or @sleep <= 0 or @activity <= 0
     erb(:failure)
   else
     erb(:index)
   end
+end
+post('/play') do
+  @name=$new_tamagotchi.name()
+  $new_tamagotchi.play()
+  @food = $new_tamagotchi.food_level()
+  @sleep = $new_tamagotchi.sleep_level()
+  @activity = $new_tamagotchi.activity_level()
 
+  if @food <= 0 or @sleep <= 0 or @activity <= 0
+    erb(:failure)
+  else
+    erb(:index)
+  end
+end
+post('/sleep') do
+  @name=$new_tamagotchi.name()
+  $new_tamagotchi.make_sleep()
+  @food = $new_tamagotchi.food_level()
+  @sleep = $new_tamagotchi.sleep_level()
+  @activity = $new_tamagotchi.activity_level()
+
+  if @food <= 0 or @sleep <= 0 or @activity <= 0
+    erb(:failure)
+  else
+    erb(:index)
+  end
 end
